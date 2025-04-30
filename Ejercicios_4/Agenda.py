e1c862abb0aac91358dda694facb47a01e3970da
marianarestrepo26 <marianartpo08@gmail.com>
2025-04-30 15:18:50 -0500

    Python

--- Ejercicios_4/Agenda.py
+++
@@ -1,8 +0,0 @@
-#Crea una pequeña agenda que permita:
-#Agregar un nuevo contacto (nombre y número de teléfono).
-#Buscar un contacto por su nombre.
-#Mostrar todos los contactos.
-#Eliminar un contacto.
-#Requisitos:
-#Usar un diccionario donde el nombre sea la clave y el número sea el valor.
-#Crear un pequeño menú en consola para elegir las acciones.
